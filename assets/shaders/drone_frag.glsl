a0c44a5d34b3731817070d59a7e9b3ddd13c4c80
Your Name <you@example.com>
2021-07-06 15:33:02 +0200

    Mal erste Versuche die Drohne zu laden. Leider erfolglos. Auch der Versusch, eine ältere TRon-Shader version zu nutzen hat keinen Erfolg gebracht. Aber immerhin läuft das Programm trotzdem noch :D Für die WOlken muss ein anderer Shader erstellt werden, da sie keine Texturkoordinaten haben! Ups, den neuen Teil vergessen :D

---
+++ assets/shaders/drone_frag.glsl
@@ -0,0 +1,32 @@
+#version 330 core
+
+//input from vertex shader
+in struct VertexData
+{
+    vec3 position;
+    vec2 texture;
+    vec3 normale;
+    vec3 toPointLight;
+} vertexData;
+
+uniform sampler2D diff;
+uniform sampler2D emit;
+uniform sampler2D specular;
+uniform float shininess;
+
+//fragment shader output
+out vec4 color;
+
+
+void main(){
+
+    vec3 n=normalize(vertexData.normale);
+
+    //Texturverarbeitung
+    vec3 diffCol = texture(diff, vertexData.texture).rgb;
+    vec3 emitCol = texture(emit, vertexData.texture).rgb;
+    vec3 specularCol = texture(specular, vertexData.texture).rgb;
+
+    color = vec4(emitCol,1.0);
+
+}
---
+++ assets/shaders/drone_vert.glsl
@@ -0,0 +1,40 @@
+#version 330 core
+
+layout(location = 0) in vec3 position;
+layout(location = 1) in vec2 tc;
+layout(location = 2) in vec3 normale;
+
+//uniforms
+// translation object to world
+uniform mat4 model_matrix;
+uniform mat4 view_matrix;
+uniform mat4 proj_matrix;
+//für Textur
+uniform vec2 tcMultiplier;
+
+out struct VertexData
+{
+    vec3 position;
+    vec2 texture;
+    vec3 normale;
+    vec3 toPointLight;
+} vertexData;
+
+//
+void main(){
+    /*vec4 pos = model_matrix * vec4(position, 1.0f);
+    vec4 nor = vec4(normale,0.0f);*/
+
+    //Spieleransicht
+    mat4 modelView = view_matrix * model_matrix;
+    //Postiton an View angepasst
+    vec4 pos =  modelView * vec4(position, 1.0f);
+    vec4 nor = inverse(transpose(modelView)) * vec4(normale, 0.0f);
+
+    gl_Position = proj_matrix * pos;
+    vertexData.position = -pos.xyz;
+    vertexData.texture = tc * tcMultiplier;
+    vertexData.normale = nor.xyz;
+
+}
+
