3d9871baebf6cd0fade5e433b981f38869891178
Your Name <you@example.com>
2021-07-17 14:41:24 +0200

    Ring dazu geladen.

--- src/main/kotlin/cga/exercise/game/Scene.kt
+++ src/main/kotlin/cga/exercise/game/Scene.kt
@@ -26,36 +26,32 @@ import kotlin.math.sin
  */
 class Scene(private val window: GameWindow) {
     private val staticShader: ShaderProgram
-    //Shader für Drohne und Wolken. Leider funktioniert noch nicht alles so wie es sollte...
     private val droneShader:ShaderProgram
-    private val cloudShader:ShaderProgram //TODO: Shader muss noch angepasst werde. Eventuell wegen der ViewMatrix o.ä. schauen?
+    private val cloudShader:ShaderProgram
 
 
     //ObjectLoader
 
      //Object laden
     private val resGround : OBJLoader.OBJResult = OBJLoader.loadOBJ("assets/models/ground.obj")
-    private val resDrone:OBJLoader.OBJResult=OBJLoader.loadOBJ("assets/models/drone.obj")
-    private val resCloud:OBJLoader.OBJResult=OBJLoader.loadOBJ("assets/models/cloud.obj")
+    private val resCloud:OBJLoader.OBJResult=OBJLoader.loadOBJ("assets/cloud/cloud.obj")
+    private val resRing:OBJLoader.OBJResult=OBJLoader.loadOBJ("assets/ring/checkpoint ring2.obj")
 
      //Mesh für die Daten von Vertex und Index laden
     private val objMeshGround : OBJLoader.OBJMesh = resGround.objects[0].meshes[0]
-    private val objMeshDrone:OBJLoader.OBJMesh=resDrone.objects[0].meshes[0]
     private val objMeshCloud:OBJLoader.OBJMesh=resCloud.objects[0].meshes[0]
+    private val objMeshRing:OBJLoader.OBJMesh=resRing.objects[0].meshes[0]
 
     //Meshes
     private var groundMesh : Mesh
-    private var droneMesh:Mesh
     private val cloudMesh:Mesh
 
-    //TODO: eventuell das Objekt in seperaten Ordner und an ModellLoder anpassen?
     private var cycleRend = ModelLoader.loadModel("assets/Light Cycle/Light Cycle/HQ_Movie cycle.obj", Math.toRadians(-90.0f),Math.toRadians(90.0f), 0.0f) ?: throw IllegalArgumentException("Could not load the model")
-    private var drone2Rend = ModelLoader.loadModel("assets/drone/drone.obj",Math.toRadians(-90.0f),Math.toRadians(90.0f), 0.0f) ?: throw IllegalArgumentException("Could not load the model")
-    //private var cloudRend=ModelLoader.loadModel("assets/models/cloud.obj",0.0f,0.0f,0.0f)?: throw IllegalArgumentException("Could not load the model")
+    private var droneRend = ModelLoader.loadModel("assets/drone/drone.obj",Math.toRadians(0.0f),Math.toRadians(90.0f), 0.0f) ?: throw IllegalArgumentException("Could not load the model")
+    private var ring2Rend=ModelLoader.loadModel("assets/ring/checkpoint ring2.obj",Math.toRadians(0.0f),Math.toRadians(90.0f), 0.0f) ?: throw IllegalArgumentException("Could not load the model")
 
     //Renderables
     private var groundRend = Renderable()
-    private val droneRend=Renderable()
     private val cloudRend= Renderable()
 
     //Camera
@@ -80,10 +76,10 @@ class Scene(private val window: GameWindow) {
         //initial opengl state
         //glClearColor(0.6f, 1.0f, 1.0f, 1.0f); GLError.checkThrow()
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f); GLError.checkThrow()
-        //glDisable(GL_CULL_FACE); GLError.checkThrow()
-        //glEnable(GL_CULL_FACE)
-        //glFrontFace(GL_CCW)
-        //glCullFace(GL_BACK)
+        glDisable(GL_CULL_FACE); GLError.checkThrow()
+        glEnable(GL_CULL_FACE)
+        glFrontFace(GL_CCW)
+        glCullFace(GL_BACK)
         glEnable(GL_DEPTH_TEST); GLError.checkThrow()
         glDepthFunc(GL_LESS); GLError.checkThrow()
 
@@ -109,18 +105,11 @@ class Scene(private val window: GameWindow) {
         val diffTex = Texture2D("assets/textures/ground_diff.png", true)
         val specTex = Texture2D("assets/textures/ground_spec.png", true)
 
-        val droneEmTex = Texture2D("assets/textures/drone_Metallic.png", true)
-        val droneMeTex = Texture2D("assets/textures/drone_Emission.png", true)
-        val droneRoTex = Texture2D("assets/textures/drone_Roughness.png", true)
-        val droneAlTex = Texture2D("assets/textures/drone_Albedo.png", true)
-        val droneNoTex = Texture2D("assets/textures/drone_Normal.png", true)
-        val droneOcTex = Texture2D("assets/textures/drone_Occlusion.png", true)
+        val cloudTex=Texture2D("assets/cloud/white.tga",true)
 
         //erzeuegn
         val groundMaterial = Material(diffTex, emitTex, specTex, 60.0f, Vector2f(64.0f,64.0f))
-         //TODO: Material richtig auf die Drohne laden :D Shader mit mehr Texturvariablen? neues Material?
-        //val droneMaterial=Material(droneMeTex,droneEmTex,droneRoTex,60.0f, Vector2f(64.0f,64.0f))
-        val droneMaterial=droneMaterial(droneAlTex,droneEmTex,droneMeTex,droneNoTex,droneOcTex,droneRoTex,60.0f, Vector2f(64.0f,64.0f))
+        val cloudMaterial=Material(cloudTex,cloudTex,cloudTex,60.0f, Vector2f(64.0f,64.0f))
 
         //Texturparameter für Objektende
         emitTex.setTexParams(GL_REPEAT, GL_REPEAT, GL11.GL_LINEAR_MIPMAP_LINEAR, GL_LINEAR)     //Linear = zwischen farbwerten interpolieren
@@ -129,28 +118,23 @@ class Scene(private val window: GameWindow) {
 
         //Mesh erzeugen
         groundMesh = Mesh(objMeshGround.vertexData, objMeshGround.indexData, vertexAttributes, groundMaterial)
-        //droneMesh= Mesh(objMeshDrone.vertexData,objMeshDrone.indexData,vertexAttributes,droneMaterial)
-        //Material für die Drohne
-        droneMesh= Mesh(objMeshDrone.vertexData,objMeshDrone.indexData,vertexAttributes,null, droneMaterial)
-        cloudMesh=Mesh(objMeshCloud.vertexData,objMeshCloud.indexData,cloudVertexAttributes)
+        cloudMesh=Mesh(objMeshCloud.vertexData,objMeshCloud.indexData,cloudVertexAttributes, cloudMaterial)
+
 
         //Meshes zu Randerable hinzufügen
         groundRend.meshList.add(groundMesh)
-        droneRend.meshList.add(droneMesh)
         cloudRend.meshList.add(cloudMesh)
+
 
         //Bike skalieren
         cycleRend.scaleLocal(Vector3f(0.8f))
 
-        //TODO: Drohne steht seitlich? Warum?
-
         //Drohne Skalieren/Transformieren
-        droneRend.scaleLocal(Vector3f(0.00025f)) //drone ist echt groß :D
+        droneRend.scaleLocal(Vector3f(0.00025f))
         droneRend.translateLocal(Vector3f(0.0f,10000.0f,-1.0f))
-        //Rotate funktioniert nicht wirklich? Drohne gedreht, aber auch Kamera und Bewegung verändert...
-        //droneRend.rotateAroundPoint(0.0f,Math.toRadians(90.0f),0.0f,droneRend.getWorldPosition())
-        drone2Rend.scaleLocal(Vector3f(0.00025f))
-        drone2Rend.translateLocal(Vector3f(0.0f,10000.0f,-1.0f))
+
+        ring2Rend.scaleLocal(Vector3f(0.00025f))
+        ring2Rend.translateLocal(Vector3f(-50.0f,10000.0f,-1.0f))
 
         cloudRend.scaleLocal(Vector3f(0.0025f))
         cloudRend.translateLocal(Vector3f(500.0f,50.0f,-1.0f))
@@ -182,22 +166,19 @@ class Scene(private val window: GameWindow) {
         glClear(GL_COLOR_BUFFER_BIT or GL_DEPTH_BUFFER_BIT)
         droneShader.use()
         tronCamera.bind(droneShader)
-
-        droneShader.setUniform("colorChange", Vector3f(0.0f,0.0f,1.0f))
-        //droneRend.render(droneShader)
-        drone2Rend.render(droneShader)
+        droneShader.setUniform("colorChange", Vector3f(1.0f,1.0f,1.0f))
+        droneRend.render(droneShader)
+        ring2Rend.render(droneShader)
         //shader Benutzung definieren
         staticShader.use()
         //Kamera binden
         tronCamera.bind(staticShader)
         //mesh rendern
-         //Eventuell anderer Shader nötig. Wird dargestellt, aber nicht korrekt :D
-        staticShader.setUniform("colorChange", Vector3f(1.0f))
-
+/*        staticShader.setUniform("colorChange", Vector3f(1.0f))
         staticShader.setUniform("colorChange", Vector3f(abs(sin(t)),abs(sin(t/2)),abs(sin(t/3))))
         cycleRend.render(staticShader)
         pointLight.bind(staticShader, "byklePoint")
-        frontSpotLight.bind(staticShader, "bykleSpot", tronCamera.getCalculateViewMatrix())
+        frontSpotLight.bind(staticShader, "bykleSpot", tronCamera.getCalculateViewMatrix())*/
         staticShader.setUniform("colorChange", Vector3f(0.0f,1.0f,0.0f))
         groundRend.render(staticShader)
 
